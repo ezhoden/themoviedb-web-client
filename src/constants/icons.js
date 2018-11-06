82aa79bece531f0fd65d9fdc2b489f04e39b02cc
DenisEzhov <d.ezhov@rbkmoney.com>
2018-11-06 12:46:06 +0300

    not used file removed

--- src/constants/icons.js
+++
@@ -1,6 +0,0 @@
-export default {
-    emptyFavoriteIcon: 'https://cdn4.iconfinder.com/data/icons/miu/24/common-bookmark-outline-stroke-512.png',
-    filledFavoriteIcon: 'https://cdn4.iconfinder.com/data/icons/miu/24/common-bookmark-glyph-512.png',
-    emptyStarIcon: 'https://cdn2.iconfinder.com/data/icons/ios-7-icons/50/star-128.png',
-    filledStarIcon: 'https://cdn1.iconfinder.com/data/icons/material-core/21/star-128.png'
-};
